d2687f0f846f4fe3bce21df5037a77291d78e33e
mmabubakar <almansurbukar@gmail.com>
2022-05-02 00:39:11 +0300

    Robot built complete

--- workers_day.py
+++ workers_day.py
@@ -1,6 +1,10 @@
 # Import Package(s)
 import turtle as t
 
+# Set Screen Size
+t.setup(1000,1000)
+t.title("HAPPY INTERNATIONAL WORKERS DAY 👮🏽🎖")
+
 # Create a function that does rectangle shape
 def rectangle(horizontal, vertical, color):
     t.pendown()
@@ -20,7 +24,9 @@ def rectangle(horizontal, vertical, color):
 # Setting Turtle Speed and Background Color
 t.penup()
 t.speed('slow')
-t.bgcolor('Dodger blue')
+t.bgcolor("#ecf1ff") # ecf5ff ecf1ff
+# t.bgcolor("#b8e2f2")
+# t.bgcolor('Dodger blue')
 
 # Drawing The Feet of Our Worker
 t.goto(-100,-150)
@@ -68,5 +74,21 @@ rectangle(5, 5, 'black') # The black part of the eyes (The Left Pupil)
 # Drawing The Mouth of Our Worker
 t.goto(-65, 135)
 rectangle(40, 5, 'black')
+
+t.hideturtle() # Hide The Turtle Pointer
+
+t.goto(-150, 220)
+t.pendown()
+t.color("#3e8dc0") # Set Pen Color
+t.speed(0.5)
+t.write("Happy Workers Day!", \
+         font=("Courier", 20, 'normal', 'bold'))
+t.penup()
+t.goto(-340, 200)
+t.pendown()
+t.color('Dodger blue')
+t.speed(0.5)
+t.write("'Today we celebrate your hard work & immense contribution to society'", \
+         font=("Courier", 15, 'italic'))
 
 t.hideturtle() # Hide The Turtle Pointer
